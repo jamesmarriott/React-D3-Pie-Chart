2c3de08810024bb45a208c0ace68b5d05e3088dd
James Marriott <sawheadproductions@gmail.com>
2022-02-26 11:06:52 +0000

    Clone original files

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello Ducky Candidate</h1>
+      <h2>Start coding your component</h2>
+    </div>
+  );
+}
